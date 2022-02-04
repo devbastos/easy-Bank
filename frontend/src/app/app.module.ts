700e450c86b0667de2034f840cd8aa146e696492
Gabriel Bastos <gbastosbarbosa@gmail.com>
2022-02-04 15:57:01 -0300

    projeto finalizado

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './template/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './template/header/header.component';
+import { MotivosComponent } from './template/motivos/motivos.component';
+import { ArticlesComponent } from './template/articles/articles.component';
+import { FooterComponent } from './template/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HeaderComponent,
+    MotivosComponent,
+    ArticlesComponent,
+    FooterComponent,
     
   ],
   imports: [
---
+++ frontend/src/app/template/motivos/motivos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-motivos',
+  templateUrl: './motivos.component.html',
+  styleUrls: ['./motivos.component.css']
+})
+export class MotivosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
